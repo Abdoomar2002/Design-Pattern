ec90bb186b6986d43df94c2d926c0c19deb6dca6
Abdoomar2002 <abdo20omar20@gmail.com>
2025-06-03 17:38:58 +0300

    ISP

---
+++ ISP/ISP/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+namespace ISP
+{
+    // Interface Segregation Principle (ISP) Example
+    public class Document {
+    } 
+    public interface IMachine:IPrinter, IFax, IScanner
+    {
+     
+    }
+    public interface IPrinter
+    {
+        public void Print(Document d);
+    }
+    public interface IFax
+    {
+        public void Fax(Document d);
+    }
+    public interface IScanner
+    {
+        public void Scan(Document d);
+        public void PhotoCopy(Document d);
+    }
+  
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            WriteLine("Printing document...");
+        }
+        public void Fax(Document d)
+        {
+            WriteLine("Faxing document...");
+        }
+        public void Scan(Document d)
+        {
+            WriteLine("Scanning document...");
+        }
+        public void PhotoCopy(Document d)
+        {
+            WriteLine("Photocopying document...");
+        }
+    }
+    public class SimplePrinter : IPrinter
+    {
+        public void Print(Document d)
+        {
+            WriteLine("Printing document...");
+        }
+        // Not applicable methods throw an exception
+      
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
